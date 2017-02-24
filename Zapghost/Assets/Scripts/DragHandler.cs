9d05842acc25b757906a356d9293c45251124f3e
xia4zhi3yun2 <xia4zhi3yun@gmail.com>
2017-02-23 22:08:06 -0800

    Test UI Part

---
+++ Zapghost/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour,  IDragHandler, IEndDragHandler {
+	public GameObject prefab;
+	GameObject hoverPrefab;
+
+	// Use this for initialization
+	void Start () {
+		hoverPrefab = Instantiate (prefab);
+		//RemoveScriptsFromPrefab ();
+		//AdjustPrefabAlpha ();
+		hoverPrefab.SetActive (true);
+	}
+
+	void RemoveScriptsFromPrefab() {
+		Component[] components = hoverPrefab.GetComponentsInChildren<TurretTargettingSystem>();
+		foreach (Component component in components) {
+			Destroy (component);
+		}
+	}
+
+	void AdjustPrefabAlpha() {
+		MeshRenderer[] meshRenderers = hoverPrefab.GetComponentsInChildren<MeshRenderer> ();
+		for (int i = 0; i < meshRenderers.Length; i++) {
+			Material mat = meshRenderers [i].material;
+			meshRenderers [i].material.color = new Color (mat.color.r, mat.color.g, mat.color.b, 0.5f);
+		}
+	}
+	
+	
+
+	public void OnDrag(PointerEventData eventData) {
+		// Debug.Log(eventData);
+		RaycastHit[] hits; 
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		hits = Physics.RaycastAll (ray, 50f);
+
+		if ( hits != null && hits.Length > 0) {
+			int terrainCollderQuadIndex = GetTerrainColliderQuadIndex (hits);
+			if (terrainCollderQuadIndex != -1) {
+				hoverPrefab.transform.position = hits[terrainCollderQuadIndex].point;
+				hoverPrefab.SetActive (true);
+				// Debug.Log (hits [terrainCollderQuadIndex].point);
+			} else {
+				//hoverPrefab.SetActive (false);
+			}
+		}
+	}
+
+	int GetTerrainColliderQuadIndex(RaycastHit[] hits) {
+		for (int i = 0; i < hits.Length; i++) {
+			if (hits [i].collider.gameObject.name.Equals ("Plane")) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		// If the prefab instance is active after dragging stopped, it means
+		// it's in the arena so (for now), just drop it in.
+		if (hoverPrefab.activeSelf) {
+			Instantiate (prefab, hoverPrefab.transform.position, Quaternion.identity);
+		}
+
+		// Then set it to inactive ready for the next drag!
+		hoverPrefab.SetActive (false);
+	}
+}
